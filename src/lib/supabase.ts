2ca69c58d80d3b29fbceb86a7b4814b6019bc588
ernesttanyx <ernesttanyx@gmail.com>
2026-04-28 16:27:10 +0800

    please work pls work

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,4 +1,5 @@
 import { createClient } from '@supabase/supabase-js'
+console.log("Supabase URL Check:", import.meta.env.VITE_SUPABASE_URL);
 
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
 const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY
